94011af50056176251c6a79eea62b409d33ffe05
Michael Sevilla <mikesevilla3@gmail.com>
2016-03-27 13:46:04 -0700

    trim verbosity and manage configs
    Signed-off-by: Michael Sevilla <mikesevilla3@gmail.com>

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -7,19 +7,22 @@ HADOOP_PREFIX="/usr/local/hadoop"
 NAMENODES=$($HADOOP_PREFIX/bin/hdfs getconf -namenodes)
 bin="$HADOOP_PREFIX/sbin"
 
-# Start the daemon
 HADOOP_DAEMON=${HADOOP_DAEMON,,}
+if [ "$HADOOP_DAEMON" == "config" ]; then
+  cp -r $HADOOP_CONF_DIR/* /etc/hadoop/
+  exit 0
+fi
+
+# Start the daemon
+cp -r /etc/hadoop/* $HADOOP_CONF_DIR/
 cd $HADOOP_PREFIX
 if [ "$HADOOP_DAEMON" == "namenode" ] || [ "$HADOOP_DAEMON" == "datanode" ]; then
-  bin/hadoop \
-    --config "$HADOOP_CONF_DIR" \
-    $HADOOP_DAEMON $nameStartOpt
+  bin="bin/hadoop"
 elif [ "$HADOOP_DAEMON" == "nodemanager" ] || [ "$HADOOP_DAEMON" == "resourcemanager" ]; then
-  bin/yarn \
-    --config "$HADOOP_CONF_DIR" \
-    $HADOOP_DAEMON $nameStartOpt
+  bin="bin/yarn"
 else
   echo "=> ERROR: unknown hadoop daemon type $HADOOP_DAEMON"
   exit 1
 fi
-#--hostnames "$NAMENODES" \
+
+$bin --config "$HADOOP_CONF_DIR" $HADOOP_DAEMON $nameStartOpt
